52bf3b39ebe93e7303dd8d1c25c2d94e0250b6b5
tni <tni_consultant@mail.ru>
2022-08-07 20:20:30 +0300

    first task

---
+++ src/Message.js
@@ -0,0 +1,11 @@
+import './Message.css';
+
+function Message(props) {
+    return (
+        <div className="Message">
+            <h4 className="Message__text">It's: {props.messageText}</h4>
+        </div>
+    );
+}
+
+export default Message;
