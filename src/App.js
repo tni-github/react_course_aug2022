8061f99872d8356de2108e2977a04fbb23e10c38
tni <tni_consultant@mail.ru>
2022-08-07 20:07:38 +0300

    project

---
+++ src/App.js
@@ -0,0 +1,45 @@
+/* import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+ */
+
+import './App.css';
+
+
+
+
+function App(props) {
+  return (
+    <div className="App">
+      <header className="App__header">
+        My First React App
+        <h3 className="App__header_subtext">Hello, {props.name}!</h3>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
